5d80423b144a86f2c263f1766abec7fffd533a9d
PRUDHVI-9 <prudhvirajathelli@gmail.com>
2023-04-08 18:17:59 +0530

    ADD:Assignment programs

---
+++ arrayss/src/arrayss/PrintingOfArrays.java
@@ -0,0 +1,25 @@
+package arrayss;
+
+import java.util.Scanner;
+
+public class PrintingOfArrays {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	
+	    int[] arr = new int[5];
+
+	    System.out.println("Enter integer values:");
+
+	    for(int i=0; i < arr.length; i++) {
+	       arr[i] = sc.nextInt();
+	    }
+	 
+	    System.out.println("Array elements are:");
+	    for (int i=0; i < arr.length; i++) {
+	      System.out.println(arr[i]+" ");
+	}
+	    sc.close();
+}
+	
+}
